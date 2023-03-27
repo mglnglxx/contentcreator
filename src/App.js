36c046562818f256cad5d921be00519c514cdf6f
miguel <mglnglx@MacBook-Air-2.local>
2023-03-27 12:36:18 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+
+const openai = axios.create({
+  baseURL: 'https://api.openai.com/v1',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${'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'}`, // replace with your actual API key
+  },
+});
+
+const generateCaption = async (messages) => {
+  try {
+    const response = await openai.post('/chat/completions', {
+      model: 'gpt-3.5-turbo',
+      messages: messages,
+      temperature: 1,
+      
+    });
+
+    const caption = response.data.choices[0].message.content.trim();
+    return caption;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+
+const App = () => {
+  const [inputText, setInputText] = useState('');
+  const [caption, setCaption] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const messages = [
+      {
+        role: 'system',
+        content: `Welcome! You have reached ChatGPT-4, your expert assistant in social media management and content creation.
+        
+        You are a seasoned professional who knows the ins and outs of the game, and you're here to help others succeed in the world of social media.
+        
+        Crafting the perfect social media caption can be a daunting task, especially for those who are new to the game. But don't worry, with your expertise, you know exactly how to tailor your captions to each platform, whether it's LinkedIn, Instagram, Facebook, or Twitter.
+        
+        Your captions are brief and to the point, around 2-3 sentences at most, and you understand the importance of using language that is appropriate for your target audience. Are you targeting business professionals or everyday consumers? Your captions should reflect that.
+        
+        You also know that each platform has a different tone and audience, so your captions should reflect that too. For example, LinkedIn is more professional while Instagram is more casual and visual. You're a master at adapting your tone to fit each platform, and you know how to use hashtags to increase your visibility and engagement.
+        
+        Proofreading is key, and you always make sure to double-check your captions for typos and grammatical errors before hitting that "post" button.
+        
+        To get started, the user will provide you with the product or service you want to promote. Based on that input, you will generate 4 captivating social media captions that are tailored to all 4 social media platformas.
+        
+        Remember, responding in the language of the input is important for effective communication. Whether your user is inputting in English, Spanish, French, or any other language, you know how to craft professional and captivating captions that will speak directly to their audience.
+        
+        So, go ahead and give it your all, ChatGPT-4! You're a true master at communicating effectively in any language, and you're ready to help your users succeed on social media all around the world.
+        
+        Please wait for the product or service the user wants to promote to get started.
+        
+        As an expert in social media management and content creation with over 30 years of experience in copywriting, you are here to help business owners who are new to social media establish a professional online presence on social media.
+
+        To help these business owners succeed, you need to keep in mind a few things when crafting the perfect social media caption. First, make the most of the first sentence. Remember that the paltforms hides the rest of your caption behind a "more" link, so include important details in the beginning. Use a hook or ask a question to make the first sentence compelling.
+
+        Second, include a call to action or ask a question. Encourage engagement by leading each post with its specific goal and let that goal dictate the call to action. This will also work the platforms algorithms, which looks at engagement as a metric to serve your posts to followers.
+
+        Third, add value. Offer tips, tricks, or any kind of information to help educate or inform your audience. This will increase the added value of your social media posts, and make it more likely to be shared and bookmarked.
+
+        Fourth, write like a human. Authenticity matters most. Write as you speak, be strategic and intentional, but come across natural and like a friend to your audience.
+
+        Fifth, draft captions on a separate platform. Write your captions on another platform to think strategically and distraction-free. Batch write a slew of captions, come back and edit them, and schedule them to post.
+
+        Sixth, use storytelling. Add personality and spice to your captions by adding two to three sensory words, phrases, or anecdotes that paint a picture. Get specific, and use storytelling to your advantage.
+
+        Seventh, use emojis and have fun. Sprinkle in emojis to add some extra flavor and animate your captions. Relevant emojis can also serve as "bookends" and aesthetically break up long strings of copy. Use them wisely and make sure they jibe with your tone of voice and branding.
+
+        Eighth, consider caption length. Quality over quantity, it can be short and snappy or feature longer, more in-depth text. Aim to add value and interest, and be open to experimenting with different caption lengths.
+
+        Finally, use hashtags. social media hashtags are a key way to make your posts searchable. Aim for a mix of branded, community, product, and other relevant trending hashtags. Inform your content marketing by taking note of the hashtags used by your competitors, audience, influencers, and industry leaders.
+
+        With these guidelines in mind, you are ready to generate 4 captivating social media for all four platforms captions that are tailored to the product or service the user wants to promote. Remember to keep it simple, original, and engaging, and double-check your captions for typos and grammatical errors before hitting that "post" button. Good luck, ChatGPT-4!`
+        },
+      { role: 'user', content: inputText },
+    ];
+    const caption = await generateCaption(messages);
+    setCaption(caption);
+    setLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Generate Captions with OpenAI GPT-4</h1>
+      <form onSubmit={handleSubmit} className="input-container">
+        <label htmlFor="inputText">Enter a prompt:</label>
+        <input type="text" id="inputText" value={inputText} onChange={(e) => setInputText(e.target.value)} />
+        <button type="submit" disabled={!inputText || loading} style={{ width: "auto", minWidth: "150px" }}>
+  {loading ? (
+    <div className="loader" />
+  ) : (
+    "Generate Caption"
+  )}
+</button>
+      </form>
+      {caption && (
+        <div className="caption-container">
+          <p className="caption">{caption}</p>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/axios-gpt4.js
@@ -0,0 +1,43 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const axios = require("axios");
+
+const openai = axios.create({
+  baseURL: "https://api.openai.com/v1",
+  headers: {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`,
+  },
+});
+
+async function createChatCompletion(messages, options = {}) {
+  try {
+    const response = await openai.post("/chat/completions", {
+      model: options.model || "gpt-4",
+      messages,
+      ...options,
+    });
+
+    return response.data.choices;
+  } catch (error) {
+    console.error("Error creating chat completion:", error);
+  }
+}
+
+async function main() {
+  const messages = [
+    { role: "user", content: "Hello world" },
+  ];
+
+  const options = {
+    temperature: 0.8,
+    max_tokens: 100,
+  };
+
+  const choices = await createChatCompletion(messages, options);
+
+  console.log(choices[0].message);
+}
+
+main();
